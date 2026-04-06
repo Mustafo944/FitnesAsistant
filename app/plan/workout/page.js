f3c370bfadcf2c5007070e00ace2d7017850075e
Mustafo <mustafo949@gmail.com>
2026-04-06 20:50:05 +0500

    fix: add missing useState import in workout page

--- app/plan/workout/page.js
+++ app/plan/workout/page.js
@@ -1,5 +1,6 @@
 'use client'
 
+import { useState } from 'react'
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
 import PageWrapper from '@/components/layout/PageWrapper'
 import Card from '@/components/ui/Card'
